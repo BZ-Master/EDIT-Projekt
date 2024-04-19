75658dcd5654206943c6002d9e79a2f85beb212e
BZ-Master <bartul.zavorovic@gmail.com>
2024-04-19 22:12:50 +0200

    minorno

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -1,12 +1,26 @@
 M.AutoInit()
 
-
 let players = []
 let tablica = document.getElementById("ljestvica")
 
 function processPlayers() {
+    let t
+    for (let i = 0; i < players.length; i++) {
+        for (let j = i + 1; j < players.length; j++) {
+            if (players[i].score < players[j].score) {
+                t = players[i]
+                players[i] = players[j]
+                players[j] = t
+            }
+        }
+    }
+
     players.forEach(el => {
-        console.log(el)
+        tablica.innerHTML += `
+        <tr>
+            <td>${el.username}</td>
+            <td>${el.score}</td>
+        </tr>`
     })
 }
 
@@ -18,38 +32,9 @@ function getPlayers() {
                 score: doc.data().score
             })
         })
-    })).then(processPlayers())
-
-    console.log(players)
-
-    // for (let i = 0; i < players.length; i++) {
-    //     for (let j = i; j < players.length; j++) {
-    //         if (players[i].score > players[j].score){
-    //             players[i], players[j] = players[j], players[i]
-    //         }
-    //     }
-    // }
+    })).then(() => {
+        processPlayers()
+    })
 }
 
 getPlayers()
-
-
-// players = getPlayers(players)
-// players.forEach(el => {
-//     tablica.innerHTML += `
-//     <tr>
-//         <td>${el.username}</td>
-//         <td>${el.score}</td>
-//     </tr>`
-// })
-
-// players.forEach(el => {
-//     console.log(el)
-//     // tablica.innerHTML += `
-//     // <tr>
-//     //     <td>${el.username}</td>
-//     //     <td>${el.score}</td>
-//     // </tr>` 
-// })
-
-
--- js/login.js
+++ js/login.js
@@ -12,90 +12,61 @@ const firebaseConfig = {
 const app = firebase.initializeApp(firebaseConfig);
 let database = app.firestore()
 
-function otvoriPrijavu() {
-    let okvir = document.getElementById("okvir")
 
-    okvir.innerHTML = `
-    <div class="container">
-        <h5 class="center">PRIJAVA</h5>
-        <p class="center">Unesite korisničko ime i lozinku. Ako želite kreirati račun kliknite REGISTER!<p>
-        <div class="row">
-            <div class="col s12 m6 l4 offset-m3 offset-l4">
-                <form>
-                    <div class="row">
-                        <div class="input-field col s12">
-                            <input placeholder="Username" id="username" type="text" class="validate center-align">
-                        </div>
-                    </div>
+let okvir = document.getElementById("okvir")
 
-                    <div class="row">
-                        <div class="input-field col s12">
-                            <input placeholder="Password" id="password" type="password" class="validate center-align">
-                        </div>
-                    </div>
-                </form>
-                <div class="row">
-                    <button class="btn waves-effect waves-light grey darken-2 col s6 m4 l4 offset-m4 offset-l4 offset-s3" id="login">Login</button>
-                </div>
-                <div class="row">
-                    <button class="btn waves-effect waves-light grey darken-2 col s6 m4 l4 offset-m4 offset-l4 offset-s3" id="register">Register</button>
-                </div>
-            </div>
-        </div>
-    </div>`
+document.getElementById("login").addEventListener("click", () => {
+    let name = document.getElementById("username").value
+    let pass = document.getElementById("password").value
 
-    document.getElementById("login").addEventListener("click", () => {
-        let name = document.getElementById("username").value
-        let pass = document.getElementById("password").value
+    let prijava = false
 
-        let prijava = false
+    database.collection("Korisnici").where("username", "==", name).where("password", "==", pass).get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            prijava = true
+        })
 
-        database.collection("Korisnici").where("username", "==", name).where("password", "==", pass).get().then(function (querySnapshot) {
-            querySnapshot.forEach(function (doc) {
-              prijava = true
-            })
-
-            if (prijava) {
-                alert("Uspjesna prijava")
-                okvir.innerHTML = `
+        if (prijava) {
+            alert("Uspjesna prijava")
+            okvir.innerHTML = `
                 <br>
                 <div class="row center">
                     <a class="waves-effect waves-light btn grey darken-2" onclick=postaviIgru()>Pokreni igru</a>
                 </div>`
-            }
+        }
 
-            else {
-                alert("Pogresno ime ili lozinka!")
+        else {
+            alert("Pogresno ime ili lozinka!")
+        }
+    })
+})
+
+document.getElementById("register").addEventListener("click", () => {
+    let name = document.getElementById("username").value
+    let pass = document.getElementById("password").value
+
+    let korisnici = database.collection("Korisnici")
+
+    let novi = true
+
+    korisnici.where("username", "==", name).get().then((querySnapshot => {
+        querySnapshot.forEach(doc => {
+            if (doc.data().username == name) {
+                novi = false
             }
         })
-    })
 
-    document.getElementById("register").addEventListener("click", () => {
-        let name = document.getElementById("username").value
-        let pass = document.getElementById("password").value
-
-        let korisnici = database.collection("Korisnici")
-
-        let novi = true
-
-        korisnici.where("username", "==", name).get().then((querySnapshot => {
-            querySnapshot.forEach(doc => {
-                if (doc.data().username == name) {
-                    novi = false
-                }
+        if (novi) {
+            korisnici.add({
+                username: name,
+                password: pass,
+                score: 0
             })
+            alert(`Uspješna registracija! Dobro došli ${name}!`)
+        }
 
-            if (novi) {
-                korisnici.add({
-                    username: name,
-                    password: pass,
-                    score: 0
-                })
-            }
-
-            else {
-                alert("Korisnik s tim imenom već postoji!")
-            }
-        }))
-    })
-}
+        else {
+            alert("Korisnik s tim imenom već postoji!")
+        }
+    }))
+})
