f98ed52e1177acab104f52a4e1c1ded66aec3b40
BZ-Master <bartul.zavorovic@gmail.com>
2024-04-23 21:57:59 +0200

    almost done i hope

--- js/login.js
+++ js/login.js
@@ -14,8 +14,13 @@ let database = app.firestore()
 
 let player = {
     coins: 0,
-    damage: 10
+    damage: 10,
+    shield: 0.1,
+    heal: 10,
+    super: 2
 }
+
+let okvir = document.getElementById("game")
 
 function setupShop() {
     let shop = document.getElementById("upgrades")
@@ -23,27 +28,76 @@ function setupShop() {
     shop.innerHTML = `
     <h3 class="center">Upgrades</h3>
     <div class="row">
-        <div id="coinCounter" class="card col s4 m2 l1 center">
+        <div id="coinCounter" class="card col s4 m2 l2 center">
             <p><i class="material-icons">attach_money</i>${player.coins}</p>
         </div>
     </div>
     
     <div class="row">
-        <div class="col s6 m3 l3">
+        <div class="col s12 m6 l6 offset-m3 offset-l3">
             <div class="card">
-                <div class="card-content">
+                <div class="card-content center">
                     <h6 class="center"><b>ATTACK</b></h6>
-                        <p><i>Description:</i> Napadni neprijatelja </p>
-                        <p><i>Damage:</i> ${player.damage}</p>
-                        <p><i>Cijena:</i> 100</p>
+                        <p><b>Description:</b><br>Napadni neprijatelja. </p>
+                        <p><b>Damage:</b><br>${player.damage}</p>
+                        <p><b>Cost:</b><br>${(player.damage) * 10}</p>
+                </div>
+                <a class="waves-effect waves-light btn col s12"><i class="material-icons left">arrow_upward</i>upgrade</a>
+            </div>
+        </div>
+
+        <div class="col s12 m6 l6 offset-m3 offset-l3">
+            <div class="card">
+                <div class="card-content center" id="shieldCard">
+                    <h6 class="center"><b>SHIELD</b></h6>
+                        <p><b>Description:</b><br>Štiti od neprijateljske štete tijekom jedne runde.</p>
+                        <p><b>Level:</b><br>${player.shield * 10} (max 10)</p>
+                        <p><b>Shield percent:</b><br>${player.shield*100}%</p>
+                        <p><b>Cost:</b><br>${(player.shield) * 100}</p>
+                </div>
+                <a class="waves-effect waves-light btn col s12" id="shieldUpgradeButton"><i class="material-icons left" >arrow_upward</i>upgrade</a>
+            </div>
+        </div>
+
+        <div class="col s12 m6 l6 offset-m3 offset-l3">
+            <div class="card">
+                <div class="card-content center">
+                    <h6 class="center"><b>HEAL</b></h6>
+                        <p><b>Description:</b><br>Povećaj si health.</p>
+                        <p><b>Level:</b><br>${player.heal / 10}</p>
+                        <p><b>Heal:</b><br>${player.heal}</p>
+                        <p><b>Cost:</b><br> ${(player.heal) * 10}</p>
+                </div>
+                <a class="waves-effect waves-light btn col s12"><i class="material-icons left">arrow_upward</i>upgrade</a>
+            </div>
+        </div>
+
+        <div class="col s12 m6 l6 offset-m3 offset-l3">
+            <div class="card">
+                <div class="card-content center">
+                    <h6 class="center"><b>SUPER ATTACK</b></h6>
+                        <p><b>Description:</b><br>Napravi višestruko veći damage odjednom. Puni se nakon 3 napada. </p>
+                        <p><b>Level:</b><br>${player.super}</p>
+                        <p><b>Attack multiplier:</b><br>${player.super}</p>
+                        <p><b>Cost:</b><br>${player.super * 100}</p>
                 </div>
                 <a class="waves-effect waves-light btn col s12"><i class="material-icons left">arrow_upward</i>upgrade</a>
             </div>
         </div>
     </div>`
-}
 
-let okvir = document.getElementById("okvir")
+    if (player.shield == 1) {
+        let botun = document.getElementById("shieldUpgradeButton")
+        botun.classList.add("disabled")
+
+        let kartica = document.getElementById("shieldCard")
+        kartica.innerHTML = `                    
+        <h6 class="center"><b>SHIELD</b></h6>
+            <p><b>Description:</b><br>Štiti od neprijateljske štete tijekom jedne runde.</p>
+            <p><b>Level:</b><br> 10 (MAX)</p>
+            <p><b>Shield percent:</b><br>${player.shield * 100}% </p>`
+    }
+}
 
 document.getElementById("login").addEventListener("click", () => {
     let name = document.getElementById("username").value
@@ -55,7 +109,10 @@ document.getElementById("login").addEventListener("click", () => {
         querySnapshot.forEach((doc) => {
             prijava = true
             player.coins = doc.data().coins
-            player.damage = doc.data().damageLevel*10
+            player.damage = doc.data().damageLevel * 10
+            player.shield = doc.data().shieldLevel * 0.1
+            player.heal = doc.data().healLevel * 10
+            player.super = doc.data().superLevel
         })
 
         if (prijava) {
@@ -95,7 +152,10 @@ document.getElementById("register").addEventListener("click", () => {
                 password: pass,
                 score: 0,
                 coins: 0,
-                damageLevel: 0
+                damageLevel: 1,
+                shieldLevel: 1,
+                healLevel: 1,
+                superLevel: 1
             })
 
             alert(`Uspješna registracija! Dobro došli ${name}!`)
